50f777dcff43b8abce7df6c7e18cabfe14392cc2
quaid <quaidperkins@gmail.com>
2023-05-16 21:40:24 +0100

    Fabric 1.19.4

--- src/main/java/io/github/lieonlion/lolgilded/GildedArmor.java
+++ src/main/java/io/github/lieonlion/lolgilded/GildedArmor.java
@@ -2,12 +2,18 @@ package io.github.lieonlion.lolgilded;
 
 import io.github.lieonlion.lolgilded.init.ItemInit;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GildedArmor implements ModInitializer {
 	public static final String MODID = "lolgilded";
 	public static final Logger LOGGER = LoggerFactory.getLogger("lolgilded");
+
+	public static final TagKey<Item> GILDED_ARMOR = TagKey.of(RegistryKeys.ITEM, new Identifier("lolgilded", "gilded_armor"));
 
 	@Override
 	public void onInitialize() {
--- src/main/java/io/github/lieonlion/lolgilded/mixin/PiglinBrainMixin.java
+++ src/main/java/io/github/lieonlion/lolgilded/mixin/PiglinBrainMixin.java
@@ -1,10 +1,8 @@
 package io.github.lieonlion.lolgilded.mixin;
 
-import io.github.lieonlion.lolgilded.init.ArmorMaterialInit;
-import io.github.lieonlion.lolgilded.init.TagInit;
+import io.github.lieonlion.lolgilded.GildedArmor;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.PiglinBrain;
-import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,6 +18,6 @@ import java.util.Iterator;
 public abstract class PiglinBrainMixin {
     @Inject(method = "wearsGoldArmor", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
     private static void wearsGoldArmor(LivingEntity entity, CallbackInfoReturnable<Boolean> info, Iterable<ItemStack> iterable, Iterator iterator, ItemStack stack, Item item) {
-        if (stack.isIn(TagInit.GILDED_ARMOR)) info.setReturnValue(true);
+        if (stack.isIn(GildedArmor.GILDED_ARMOR)) info.setReturnValue(true);
     }
 }
